ef9733b61f720f1c03bb3afe6bd46cfe494f5b77
adeabreu <0deabreubarbara@gmail.com>
2024-04-16 22:17:10 -0500

    Creando y modificando las rutas de trayectorias

--- src/routes/trajectoriesRoutes.ts
+++ src/routes/trajectoriesRoutes.ts
@@ -1,7 +1,24 @@
 import express from 'express'
-import { getLocationByDate } from '../controllers/trajectoriesController'
+import {
+    createTrajectory,
+    getTrajectories,
+    getTrajectoriesByTaxiId,
+    getLocationByDate,
+    /* modifyTrajectory, */
+    deleteTrajectory,
+} from '../controllers/trajectoriesController'
 const router = express.Router()
 
+router.post('/', createTrajectory)
+
+router.get('/', getTrajectories)
+
+router.get('/:taxiId', getTrajectoriesByTaxiId)
+
 router.get('/:taxiId/:date', getLocationByDate)
+
+// router.put('/:taxiId/:date', modifyTrajectory)
+
+router.delete('/:trajectoryId', deleteTrajectory)
 
 export default router
