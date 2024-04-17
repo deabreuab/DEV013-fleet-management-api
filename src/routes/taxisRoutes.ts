b7fdb63e97eec39bacacf88eb3370de98f898c88
Barbara De Abreu <104578210+deabreuab@users.noreply.github.com>
2024-04-16 20:20:31 -0500

    Agregando rutas de taxis para post, get, patch y delete

--- src/routes/taxisRoutes.ts
+++ src/routes/taxisRoutes.ts
@@ -1,8 +1,22 @@
 import express from 'express'
-import { getTaxis } from '../controllers/taxisController'
+import {
+    registerTaxi,
+    getTaxis,
+    getTaxiById,
+    modifyTaxi,
+    deleteTaxi,
+} from '../controllers/taxisController'
 
 const router = express.Router()
 
+router.post('/', registerTaxi)
+
 router.get('/', getTaxis)
+
+router.get('/:taxiId', getTaxiById)
+
+router.patch('/:taxiId', modifyTaxi)
+
+router.delete('/:taxiId', deleteTaxi)
 
 export default router
