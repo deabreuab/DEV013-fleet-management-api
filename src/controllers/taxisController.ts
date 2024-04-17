7b5da231b657b557a55dc1030bbe4f99b67f8afd
Barbara De Abreu <104578210+deabreuab@users.noreply.github.com>
2024-04-16 20:21:43 -0500

    Creando las funciones para crear, obtener, modificar y eliminar taxis

--- src/controllers/taxisController.ts
+++ src/controllers/taxisController.ts
@@ -1,6 +1,25 @@
 import type { RequestHandler } from 'express' // En Express, RequestHandler es un tipo de dato que describe una función que puede manejar una solicitud HTTP. Ya maneja tipo Request, Response y devuelve 'void'
 import prisma from '../config/connect'
 
+const registerTaxi: RequestHandler = async (req, res) => {
+    try {
+        const { id, plate } = req.body
+        const result = await prisma.taxis.create({
+            data: {
+                id,
+                plate
+            }
+        })
+        res.json({
+            message: 'Successful operation',
+            data: result,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send()
+    }
+}
+
 const getTaxis: RequestHandler = async (req, res) => {
     try {
         const { page = 1, limit = 10 } = req.query
@@ -21,4 +40,68 @@ const getTaxis: RequestHandler = async (req, res) => {
     }
 }
 
-export { getTaxis }
+const getTaxiById: RequestHandler = async (req, res) => {
+    try {
+        const { taxiId } = req.params
+        if (!taxiId) {
+            return res.status(400).json({
+                message: 'Invalid Id, please enter a valid one'
+            })
+        }
+        const result = await prisma.taxis.findUnique({
+            where: {
+                id: +taxiId
+            }
+        })
+        res.json({
+            message: 'Successful operation',
+            data: result,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send()
+    }
+}
+
+const modifyTaxi: RequestHandler = async (req, res) => {
+    try {
+        const { taxiId } = req.params
+        const { id, plate } = req.body
+        const result = await prisma.taxis.update({
+            where: {
+                id: +taxiId
+            },
+            data: {
+                id,
+                plate
+            }
+        })
+        res.json({
+            message: 'Changes made successfully',
+            data: result,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send()
+    }
+}
+
+const deleteTaxi: RequestHandler = async (req, res) => {
+    try {
+        const { taxiId } = req.params
+        const result = await prisma.taxis.delete({
+            where: {
+                id: +taxiId
+            }
+        })
+        res.json({
+            message: 'Data deleted successfully',
+            data: result,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send()
+    }
+}
+
+export { registerTaxi, getTaxis, getTaxiById, modifyTaxi, deleteTaxi }
