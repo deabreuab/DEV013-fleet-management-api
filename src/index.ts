8f02fe1920908c5de906750528a2b100831f6e69
Barbara <0deabreubarbara@gmail.com>
2024-04-20 21:22:33 -0500

    agregando api docs como middleware para mostrar documentacion swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
 import router from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerOutput from './config/swagger_output.json'
+
 const app = express()
 const PORT = process.env.PORT ?? 3001
 
 app.use(express.json())
 app.use('/api', router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput as swaggerUi.JsonObject))
 
 app.get('/', (req, res) => {
     res.send('HELLOOOO')
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import trajectories from './trajectoriesRoutes'
 const router = express.Router()
 
 router.use('/taxis', taxis)
+
 router.use('/trajectories', trajectories)
 
 export default router
