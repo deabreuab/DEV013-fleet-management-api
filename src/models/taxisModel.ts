eb2bd2beeb4a7ad6a0898a5e41c37e3f577ff924
Barbara <0deabreubarbara@gmail.com>
2024-04-25 10:06:32 -0500

    creacion de modelos para exportar interfaces de typescript

---
+++ src/models/taxisModel.ts
@@ -0,0 +1,9 @@
+export interface ITaxi {
+    id: number
+    plate: string
+}
+
+export interface IPaginated {
+    page?: number
+    limit?: number
+}
---
+++ src/models/trajectoriesModel.ts
@@ -0,0 +1,7 @@
+export interface ITrajectory {
+    id: number
+    taxi_id: number
+    latitude?: number | null
+    longitude?: number | null
+    date?: Date | null
+}
