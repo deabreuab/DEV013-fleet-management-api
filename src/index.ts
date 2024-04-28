b21937ed3b4ce44b2fe91984bf56cdcda8f0a32d
Barbara <0deabreubarbara@gmail.com>
2024-04-28 18:00:34 -0500

    se agrego al comando de test un NODE_ENV para evitar el error de jest a la hora de ejecutar varios test (port already in use)

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,10 @@ app.get('/', (req, res) => {
     res.send('HELLOOOO')
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
 
 export default app
